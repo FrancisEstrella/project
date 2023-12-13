7ac38aea28a6551a65463ad545c212f3ac68827b
FrancisEstrella <143943671+FrancisEstrella@users.noreply.github.com>
2023-12-13 21:54:30 +0800

    Add files via upload

---
+++ js/modal2.js
@@ -0,0 +1,59 @@
+const modal_1=document.getElementsByClassName("modal-1")[0]; // The first page
+const modal_2= document.getElementsByClassName("modal-2")[0]; // The second page
+const modal_3= document.getElementsByClassName("modal-3")[0]; // The losing status page
+const text_field=document.getElementsByClassName("text-field")[0]; //the name of the person entered 
+const submit_button=document.getElementsByClassName("submit_button")[0]; // the submit button
+const start_button=document.getElementsByClassName("start_button")[0]; //the game start button
+const restart_button=document.getElementsByClassName("restart_button")[0]; //the game restart button
+const userName=document.getElementsByClassName("username")[0];// the username in the second screen
+const user_screen=document.getElementsByClassName("user-screen")[0];//user name that has to be displayed on the top of the game screen
+const score_screen=document.getElementsByClassName("score-screen")[0];// score plate at the top
+const canvas=document.getElementsByClassName("canvas")[0];
+const wrongInput =document.querySelector(".wrongInput");
+
+
+userName.innerHTML=text_field.value;
+modal_2.style.display="none";
+modal_3.style.display="none";
+canvas.style.display="none";
+
+
+const onNameSubmit =()=>{
+
+      if(text_field.value!=""){
+        console.log("efe");
+         userName.innerHTML=text_field.value;
+         modal_1.style.display="none";
+         modal_2.style.display="block";
+
+       }else{
+
+       wrongInput.innerHTML="please enter ur name";
+        //we wil show this as a error message on screen later
+       }
+}
+
+const onGameStart= () =>
+{
+  setGame();
+
+  user_screen.innerHTML="Hey "+text_field.value;
+
+  user_screen.innerHTML="Hey , "+text_field.value;
+  score_screen.innerHTML="Score:"+points;
+  canvas.style.display="block";
+  modal_2.style.display="none";
+}
+
+const onGameRestart =() =>
+{
+  setGame();
+  modal_3.style.display="none";
+  score=0;
+  score_screen.innerHTML="Score:"+points;
+
+}
+
+submit_button.addEventListener("click",onNameSubmit);
+start_button.addEventListener("click",onGameStart);
+restart_button.addEventListener("click",onGameRestart);
---
+++ js/snake2.js
@@ -0,0 +1,49 @@
+
+const status=document.getElementsByClassName("status")[0];
+const userName_2=document.getElementsByClassName("username")[1]; 
+
+//for audio
+let dead = new Audio();
+let eat = new Audio();
+let left =new Audio();
+let right =new Audio();
+let up =new Audio();
+let down =new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+left.src = "audio/left.mp3";
+right.src = "audio/right.mp3";
+up.src = "audio/up.mp3";
+down.src = "audio/down.mp3";
+
+
+
+let gameOn=true;  //flag to know when we have to start the game
+let btnP;  //this is to set the game interval
+let temp_interval;
+let points=0;
+let positionX=10;
+let positionY=10;
+let tcX=60;//the tiles count in x direction
+let tcY=30;//the tiles count in y direction
+let gs=20;//grid size
+let TargetX=15;
+let TargetY=15;
+let xv=0;
+let yv=0;//velocity in x and y direction
+let snake=[];
+let tail = 3;
+let firstTime=true;
+
+//because we have to keep moving the snake ,defining the velocity will be better
+//so this function changes the velocity in every direction
+let direction="right";
+let isReverseDirection =false;
+let keyPressed=false;
+
+
+
+
+
+ 
